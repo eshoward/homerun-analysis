f55703ad6a7489ac57ccbd5e837d18b5756a3ade
eshoward <evan.howard@wsu.edu>
2023-05-05 11:14:54 -0700

    trying to create heatmap.

--- homerunanalysis.R
+++ homerunanalysis.R
@@ -19,16 +19,6 @@ SEA <- SEA[colSums(!is.na(SEA)) > 0]
 
 SEA %>%  distinct(pitch_name)
 
-#chose to use -1.5 and 1.5 feet away from 0 (center of plate) because we're 
-#focusing on pitches that are hits, or near/within strike zone
-fastballs_mod <- fastballs %>%
-  filter((plate_x > -1.5) & (plate_x < 1.5))
-
-#chose to use 0.80 and 4.25 feet above the ground because we're 
-#focusing on pitches that are hits, or near/within strike zone
-fastballs_modx <- fastballs_mod %>%
-  filter((plate_z > 0.80) & (plate_z < 4.25))
-
 #used to create strike zone
 x <- c(-.95, .95, .95, -.95, -.95)
 z <- c(1.6, 1.6, 3.5, 3.5, 1.6)
@@ -43,13 +33,26 @@ ggplot()+
   geom_point(data = SEA, aes(x = plate_x,y = plate_z, color=pitch_name))+
   scale_size(range = c(0.01, 3))
 
-#used for heatmap
-location <- fastballs_modx %>% select(plate_x, plate_z)
+#creating strike zone. Below is trying to create heatmap.
+topKzone <- 3.5
+botKzone <- 1.6
+inKzone <- -0.95
+outKzone <- 0.95
 
-location <- as.data.frame(location)
+kZone <- tibble(
+    x = c(inKzone, inKzone, outKzone, outKzone, inKzone),
+    y = c(botKzone, topKzone, topKzone, botKzone, botKzone))
 
-location_melt <- melt(location)
+x <- seq(-1.5, 1.5, length.out = 50)
+y <- seq(.5, 5, length.out = 50)
+data.predict <- data.frame(X = c(outer(x,y * 0 + 1)),
+                           Z = c(outer(x * 0 + 1, y)))
+lp <- predict(fit, data.predict)
+data.predict$Probability <- exp(lp) / (1 + exp(lp))
 
+ggplot(kZone, aes(x = x, y = y))+
+  geom_tile(data = SEA, aes(x = plate_x, y = plate_z, fill = release_speed))+
+  scale_fill_distiller(palette = "Spectral")+
+  geom_path(lwd = .5, col = "black")+
+  coord_fixed()
 
-
-#create heatmap of all FF pitches
